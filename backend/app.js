4bba343bc0b70bcb4051b4ab64e8cd66be2cd627
Purvika-R <purvikar1316@gmail.com>
2026-07-12 15:22:46 +0530

    git push origin mainRevert "Completed Trip and Maintenance frontend modules"
    This reverts commit 5db8b649f4de94234d07ddbe32fc8896024d3fcf.

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
-const tripRoutes = require("./routes/tripRoutes");
-const maintenanceRoutes = require("./routes/maintenanceRoutes");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Test route
 app.get("/", (req, res) => {
     res.send("TransitOps Backend Running");
 });
-
-// API Routes
-app.use("/api/trips", tripRoutes);
-app.use("/api/maintenance", maintenanceRoutes);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,7 @@
-require("dotenv").config();
-
-const mongoose = require("mongoose");
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("✅ MongoDB Connected");
-
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("❌ MongoDB Connection Error:");
-    console.error(err);
-  });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
